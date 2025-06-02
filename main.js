98b5dbbcb0659ed5690a1ffe505e7d419a65e844
Pedro henrique <pedo.oliveira88@gmail.com>
2025-06-02 00:33:42 -0300

    criação para remover os items do array

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function showItemsList(){
     sectionList.textContent = ""
 
     items.map((item, index)=>{  
-        sectionList.innerHTML +=`
+        sectionList.innerHTML +=` 
         div class="item">
                 <div>
                     <input type="checkbox" name="list" id="item-${index}">
@@ -29,11 +29,25 @@ function showItemsList(){
                     </div>
                      <label for="item-${index}">${item.name}</label>
                     </div>
-                       <button>
+                       <button onclick = "removeItem('${item.name}')">
                     <img src="./assets/trash-icon.svg" alt="trash icon">
                 </button>
                 </div>
                   </div>
+                   
                 `
     })
+}
+
+function removeItem(itemName){
+    const itemIndex = items.findIndex((item) => item.name === itemName)
+    const divWarning = document.querySelector(".warning")
+
+    divWarning.classList.remove("hide-warning")
+   
+    if(itemIndex !== -1){
+    items.splice(itemIndex, 1)
+    }
+
+    showItemsList()
 }
